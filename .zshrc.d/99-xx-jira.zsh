7a6d26ec012af8b98e3e318b843318b41b7b7fc7
Vadim Khitrin <me@vkhitrin.com>
2025-04-13 20:22:12 +0300

    chore: Update Configuration
    Separate `xx` into individual components.

--- .zshrc.d/99-xx-jira.zsh
+++ .zshrc.d/99-xx-jira.zsh
@@ -25,7 +25,7 @@ function jiix() {
         --header-lines=1 --layout=reverse-list \
         --info=inline --color 'border:#89b4fa,label:#89b4fa,preview-fg:#89b4fa,header:#89b4fa:bold' \
         --bind="ctrl-u:become(jira open --no-browser {1} | tr -d '\n' | ${CLIPBOARD_COMMAND})" \
-        --prompt "JQL: ${JQL} " --preview="echo 'Ctrl+U: Copy URL To Clipboard | Enter: Open Issue In Browser'" \
+        --prompt "Filter " --preview="echo 'Ctrl+U: Copy URL To Clipboard | Enter: Open Issue In Browser'" \
         --preview-window=down,1,border-none \
         --bind "enter:become(jira open {1})"
 }
