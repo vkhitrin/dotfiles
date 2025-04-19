e8739ec8ce6772aa31818f8be243273ee0265130
Vadim Khitrin <me@vkhitrin.com>
2025-04-20 01:21:59 +0300

    chore: Update Configuration

--- .zshrc.d/99-xx-todo.zsh
+++ .zshrc.d/99-xx-todo.zsh
@@ -2,10 +2,15 @@ which todo > /dev/null 2>&1 || return
 
 function tdx() {
     # xx ;todo:List todo@FALSE
-     __xx_get_todo | fzf --border-label " TODO " --header-lines=1 --layout=reverse-list \
-        --info=inline --color 'border:#89b4fa,label:#89b4fa,preview-fg:#89b4fa,header:#89b4fa:bold' \
-        --prompt "Filter " --preview="echo 'CTRL+R: Refresh'" \
+     __xx_get_todo | fzf --border-label " TODO " \
+        --tmux 80% \
+        --header-lines=1 \
+        --layout=reverse-list \
+        --info=inline --color 'border:#f5c2e7,label:#f5c2e7,preview-fg:#f5c2e7,header:#f5c2e7:bold' \
+        --prompt "Filter " \
+        --preview="echo 'CTRL+R: Refresh | CTRL+N: New Todo'" \
         --preview-window=down,1,border-none \
+        --bind='ctrl-n:become:(__xx_new_todo)' \
         --bind='ctrl-r:reload:(__xx_get_todo)' \
         --bind "enter:become()"
 }
